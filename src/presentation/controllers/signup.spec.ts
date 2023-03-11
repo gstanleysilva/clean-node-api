99b04dd5ee0c54ca3f0c53af668474de540b8b7e
Gustavo Stanley <guhstanley@gmail.com>
2023-03-11 11:45:12 -0300

    fix: change controller name to SignUpController

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,8 +1,8 @@
-import { SignupController } from './signup'
+import { SignUpController } from './signup'
 
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
-    const sut = new SignupController()
+    const sut = new SignUpController()
     const httpRequest = {
       body: {
         email: 'any_email@main.com',
@@ -16,7 +16,7 @@ describe('SignUp Controller', () => {
   })
 
   test('Should return 400 if no email is provided', () => {
-    const sut = new SignupController()
+    const sut = new SignUpController()
     const httpRequest = {
       body: {
         name: 'any_name',
